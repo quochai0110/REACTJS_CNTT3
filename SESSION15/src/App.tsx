ec94892d0f94bf225608cddb172ccdeef3563ae3
C4EJS100-TrinhQuocHai <trinhquochai12@gmail.com>
2025-08-22 14:35:03 +0700

    add

---
+++ SESSION15/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Baitap1 from "./components/Baitap1";
+import Baitap2 from "./components/Baitap2";
+import Baitap3 from "./components/Baitap3";
+import Baitap4 from "./components/Baitap4";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>LUYỆN TẬP SESSION15</h1>
+        <Baitap1></Baitap1> 
+        <hr />
+        <Baitap2></Baitap2>
+        <hr />
+        <Baitap3></Baitap3>
+         <hr />
+         <Baitap4></Baitap4>
+      </div>
+    );
+  }
+}
